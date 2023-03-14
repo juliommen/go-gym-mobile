e24fbbece7f8deaf10ad6ba6b289a78f47d2d0ae
Júlio Mendonça da Costa <juliommen@gmail.com>
2023-03-14 07:40:36 -0400

    first commit

---
+++ api/src/configs/auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+  jwt: {
+    secret: "julio",
+    expiresIn: "1d"
+  },
+};
---
+++ mobile/src/storage/storageConfig.ts
@@ -0,0 +1,4 @@
+const USER_STORAGE = '@go-gym:user';
+const AUTH_STORAGE = '@go-gym:token';
+
+export { USER_STORAGE, AUTH_STORAGE };
